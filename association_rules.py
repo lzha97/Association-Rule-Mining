3960622798612102ba38dac8b12becf3897faa00
Emily Hao <emilover426@yahoo.com>
2020-04-27 01:13:30 -0400

    converted to frozenset

--- association_rules.py
+++ association_rules.py
@@ -5,31 +5,28 @@ import time
 
 t = time.time()
 
+
 def log(x, s=sys.argv[2], c = sys.argv[3]):
     print(x)
     with open('logs/'+ str(t)+'_'+str(s)+'_'+str(c)+'.txt', 'a+') as file:
         file.write(x+'\n')
 
 
-def compute_confidence(itemsets, min_conf = sys.argv[3]):
+def compute_confidence(itemsets, min_conf):
     rules = {}
     ct_rules_extracted = 0
     for iset in itemsets.keys():
-        if type(iset) == tuple:
+        if len(iset) >= 2:
             for i in iset:
-                lhs = (set(iset)).difference(set([i]))
-                if len(lhs)> 1: lhs = tuple(lhs)
-                else: lhs= tuple(lhs)[0]
+                lhs = iset.difference([i])
                 rhs = iset
-
                 numerator = itemsets[rhs]
                 denominator = itemsets[lhs]
-                score = numerator/denominator
-                if score >= float(min_conf) and (lhs,i) not in rules:
+                score = float(numerator)/float(denominator)
+                if score >= min_conf and (lhs,i) not in rules:
                         rules[(lhs,i)] = score
-                        #log('Rule: ' +  str(lhs) + '--> (' +  str(i) +  ')\t Score:' +  str(score))
                         ct_rules_extracted += 1
-    #log(str(ct_rules_extracted) + ' Rules Extracted. ')
+
     return rules, ct_rules_extracted
 
 
@@ -37,46 +34,46 @@ def compute_confidence(itemsets, min_conf = sys.argv[3]):
 def one_itemsets(items, min_sup):
     L_1 = {}
     for item in items:
-        L_1[item] = items.count(item)
+        if frozenset([item]) not in L_1:
+            L_1[frozenset([item])] = 1
+        else: L_1[frozenset([item])] += 1
+
     L_1_copy = L_1.copy()
     num_baskets = float(len(L_1.keys()))
     for item,count in L_1.items():
         if (float(count)/num_baskets < min_sup):
             del L_1_copy[item]
+
     return L_1_copy
 
 
 ### Find large itemsets
 def large_k_itemsets(items, table, min_sup):
-    #log("start of large k")
     large_itemsets = {}
     L = one_itemsets(items, min_sup) # L_(k-1): set of large itemsets that have k-1 items, L from previous iteration
     large_itemsets.update(L)
     k = 2
     while (L != {}):
         C_k = apriori_gen(list(L.keys()),k) # C_k : set of potentially large itemsets that have k number of items
-        #log("C_k is after apriori: "+str(C_k))
         for row in table:
             for c in C_k:
-                if (set(c) <= set(row)):
+                if (c <= set(row)):
                     C_k[c] += 1
-        #log("Ck after increment: " + str(C_k))
+
         L_k = {}
         for c,count in C_k.items():
             if float(count)/len(table) >= min_sup:
                 L_k[c] = count
+
         k += 1
         large_itemsets.update(L_k)
         L = L_k # L: set of large itemsets that have k number of items that meet min support out of C_k
-        #log("L_k is: "+ str(L_k))
 
     return large_itemsets
 
 
 ## takes a set of large k-1 itemsets and returns a superset of all large k-itemsets.
 def apriori_gen(prior_itemsets,k):
-    #log("in apriori")
-    #log("prior itemsets: "+str(prior_itemsets))
     #join
     C_k = set()
     for p in prior_itemsets: # L_(k-1)
@@ -86,7 +83,7 @@ def apriori_gen(prior_itemsets,k):
                 new_p.append(i)
             p = new_p
         else:
-            p = [p]
+            p = list(p)
         for q in prior_itemsets:
             new_q = []
             if isinstance(q,tuple):
@@ -94,27 +91,19 @@ def apriori_gen(prior_itemsets,k):
                     new_q.append(i)
                 q = new_q
             else:
-                q = [q]
-            if p[:-1] == q[:-1] and p[-1] != q[-1]:
-                new_itemset = tuple(set(p).union(set(q))) # a set that belongs in C_k
+                q = list(q)
+            if p[:-1] == q[:-1] and p[-1] < q[-1]:
+                new_itemset = frozenset(set(p).union(set(q))) # a set that belongs in C_k
                 if (len(new_itemset) == k):
                     C_k.add(new_itemset)
+
     #prune
-    #log("C_k is: "+ str(C_k))
     C_new = C_k.copy() # C_new is a copy of C_k for deletion during iteration.
     for c in C_k:
-        #log("c is: " + str(c))
         subsets = [list(x) for x in itertools.combinations(list(c),k-1)]
-        #log("subsets are: "+str(subsets))
         for s in subsets:
-            if len(s) == 1:
-                s = s[0]
-            else:
-                s = tuple(s)
-            #log("subset is: "+str(s))
-            if s not in prior_itemsets:
-                C_new.remove(c)
-                #log("s not in prior: "+str(s))
+            if frozenset(s) not in prior_itemsets:
+                C_new.remove(frozenset(c))
                 break
 
     return dict.fromkeys(C_new,0)
@@ -147,16 +136,16 @@ log('\n############# TABLE #############')
 log('Number of rows: '+str(len(table)))
 #log(str(table)+ '\n\n')
 ### find large itemsets
+
 itemsets = large_k_itemsets(items,table,min_sup)
 log('\n############# ITEMSETS #############\n')
 for i in itemsets:
-    log(str(i)+': '+ str(itemsets[i]))
+    log(str(set(i))+': '+ str(itemsets[i]))
 log('\nNumber of itemsets: '+str(len(itemsets)))
 
 
 print('\n############# ASSOCIATION RULES #############\n')
-high_conf_rules, ct_rules = compute_confidence(itemsets)
+high_conf_rules, ct_rules = compute_confidence(itemsets, min_conf)
 for r in high_conf_rules:
-    log('Rule: ' +  str(r[0]) + '--> (' +  str(r[1]) +  ')\t Score:' +  str(high_conf_rules[r]))
-    #log(str(i) + ' : ' + str(high_conf_rules[r]))
+    log('Rule: ' +  str(set(r[0])) + '--> (' +  str(r[1]) +  ')\t Score:' +  str(high_conf_rules[r]))
 log(str(ct_rules) + ' Rules Extracted. ')
